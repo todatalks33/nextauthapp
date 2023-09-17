4245ea59fe29a42daa185110e3a842af1d911a76
todua <todaytalks3@gmail.com>
2023-09-18 00:39:33 +0530

    commiting new changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div className='min-h-screen flex items-center justify-center'>
+    <div className='min-h-screen flex flex-col items-center justify-center'>
       <h1>Next Auth App</h1>
       <p>Created by Azal monawwar</p>
     </div>
