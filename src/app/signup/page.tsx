4c0b913c6f380fc29078ee83b6feadaeacc86a9a
todua <todaytalks3@gmail.com>
2023-09-18 01:00:55 +0530

    change type of password input

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -87,7 +87,7 @@ const SignupPage = () => {
                         className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                         id="password"
                         name="password"
-                        type="text"
+                        type="password"
                         placeholder="Enter your full name"
                         value={user.password}
                         onChange={handleChange}
